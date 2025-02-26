ac099af419952fe499e9a6b0ed927881bce3131b
vijaykanwal <vijaykanwal90@gmail.com>
2025-02-26 23:45:16 +0530

    fixed login api request

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     const userInfo = { email, password };
     console.log(userInfo)
     try {
-        const res = axios.post(`api/auth/login `,userInfo
+        const res = axios.post(`${BASE_URL}/auth/login `,userInfo
           ,{
             withCredentials: true
           }
