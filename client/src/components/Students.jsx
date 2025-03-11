5d06f4457dc605dfe9d36af04654f2a6d1d11aff
vijaykanwal <vijaykanwal90@gmail.com>
2025-03-11 09:45:09 +0530

    admin courses

--- client/src/components/Students.jsx
+++ client/src/components/Students.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BASE_URL } from "@/utils/utils";
-
+import { Button } from "../components/ui/button";
 const Students = () => {
   const [students, setStudents] = useState([]);
 
@@ -22,7 +22,11 @@ const Students = () => {
 
   return (
     <div className="container mx-auto p-6">
-      <h2 className="text-3xl font-bold text-center mb-8">Students Data</h2>
+      <div className="flex justify-between items-center"><h2 className="text-3xl font-bold text-center mb-8">Students Data</h2>
+      <Button className="mb-8" >Assign course</Button>
+      </div>
+      
+
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {students?.map((student) => (
           <div
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,4 +2,3 @@ const env = "development";
 export const BASE_URL = env === "development" ? "http://localhost:3000/api/v1" : "/api/v1";
 
 
-
