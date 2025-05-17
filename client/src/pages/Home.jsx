a43f7cf16b0cd9d9b2c95dc9979c595a8e9d5f86
vivekrawat21 <vivekrwt2111@gmail.com>
2025-05-17 10:17:04 +0530

    fix: Comment out unused imports and components in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import Partner from '../components/Partner'
-import HeroLanding from '../components/HeroLanding'
-import TestimonialBento from '@/components/TestimonialBento'
-import FAQ from '@/components/FAQ'
-import TopCourses from '@/components/TopCourses'
-import TopMentors from '@/components/TopMentors'
-import SearchCourses from '@/components/SearchCourses'
-import SpecialBundles from '@/components/SpecialBundles'
-import DiscountPopup from '@/components/DiscountPopup'
-import DownloadApp from '@/components/DownloadApp'
-import SkillUpCourses from '@/components/SkillUpCourses'
-import EducationalInstituteTestimonial from '@/components/EducationalInstituteTestimonial'
-import WebinarBanner from '@/components/WebinarBanner'
+// import Partner from '../components/Partner'
+// import HeroLanding from '../components/HeroLanding'
+// import TestimonialBento from '@/components/TestimonialBento'
+// import FAQ from '@/components/FAQ'
+// import TopCourses from '@/components/TopCourses'
+// import TopMentors from '@/components/TopMentors'
+// import SearchCourses from '@/components/SearchCourses'
+// import SpecialBundles from '@/components/SpecialBundles'
+// import DiscountPopup from '@/components/DiscountPopup'
+// import DownloadApp from '@/components/DownloadApp'
+// import SkillUpCourses from '@/components/SkillUpCourses'
+// import EducationalInstituteTestimonial from '@/components/EducationalInstituteTestimonial'
+// import WebinarBanner from '@/components/WebinarBanner'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { addMentors } from '@/features/mentors/mentorSlice'
-import { useState } from 'react';
+// import { useState } from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../utils/utils'
 
@@ -43,7 +43,7 @@ const Home = () => {
   
   return (
     <div className="min-h-screen my-0">
-      <HeroLanding/>
+      {/* <HeroLanding/>
       <Partner/>
       <SearchCourses/>
       <SpecialBundles />
@@ -55,7 +55,7 @@ const Home = () => {
       <EducationalInstituteTestimonial/>
       <TestimonialBento />
       <FAQ />
-      <DiscountPopup/>
+      <DiscountPopup/> */}
     </div>
    
   )
