757413069654e62da1b43b831a94c41665ed05fd
vijaykanwal <vijaykanwal90@gmail.com>
2025-06-10 14:55:36 +0530

    admin manage layout

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -179,7 +179,7 @@ const AdminDashboard = () => {
   </div>
 
   {/* Students Grid */}
-  <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+  <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
     {filteredStudents.map((student) => (
       <div
         key={student._id}
